cb9de1217cbd371fdcf934331d18ecbcea967073
AI22AK47 <94799989+AI22AK47@users.noreply.github.com>
2022-11-07 13:43:37 +0600

    Update main.py

--- main.py
+++ main.py
@@ -31,8 +31,7 @@ logging.info('Starting Bot...')
 # all update.message are reply from bots to user
 def start(update, context):
     update.message.reply_text(
-        "/myid - আপনার টেলিগ্রাম আইডি পান
-/mypp - আপনার টেলিগ্রাম প্রোফাইল ছবি পান")
+        "/myid - আপনার টেলিগ্রাম আইডি পান\n/mypp - আপনার টেলিগ্রাম প্রোফাইল ছবি পান")
 
 
 def cmd(update, context):
